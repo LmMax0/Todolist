3c813adb455b68d5a01eec1fa30280adf9dfea97
BackJack <3364196039@qq.com>
2020-09-21 22:39:17 +0800

    Todolist

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ Vue.use(VueRouter)
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
     {
-      path:"/login",
+      path:"/",
       name:'Login',
       component: Login
     },
